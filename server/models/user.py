6af087bc929f898f297cca837e79ef8bf172fb6c
dewll <sodiqb86@gmail.com>
2023-01-31 18:46:05 +0100

    fixed now

--- server/models/user.py
+++ server/models/user.py
@@ -68,7 +68,7 @@ class UserLogin(BaseModel):
         
         
 class UserOut(BaseModel):
-    id: PydanticObjectId
+    # id: PydanticObjectId
     first_name: Optional[str]
     last_name: Optional[str]
     bio: Optional[str]
